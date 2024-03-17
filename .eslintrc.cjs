c9491a7325fa45d146c3c3f5cc54d4269b404f85
andrewleedev2k3 <andrewleedev276@gmail.com>
2024-03-17 20:20:05 +0700

    Init Project

---
+++ .eslintrc.cjs
@@ -0,0 +1,54 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
+    'prettier'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        printWidth: 100,
+        tabWidth: 2,
+        useTabs: false,
+        semi: false,
+        bracketSpacing: true,
+        arrowParens: 'always',
+        requirePragma: false,
+        trailingComma: 'none',
+        endOfLine: 'auto'
+      }
+    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 0,
+    'react/display-name': 0,
+    'no-console': 1,
+    'no-lonely-if': 1,
+    'no-unused-vars': 1,
+    'no-trailing-spaces': 1,
+    'no-multi-spaces': 1,
+    'no-multiple-empty-lines': 1,
+    'space-before-blocks': ['error', 'always'],
+    'object-curly-spacing': [1, 'always'],
+    indent: ['warn', 2],
+    semi: [1, 'never'],
+    quotes: ['error', 'single'],
+    'array-bracket-spacing': 1,
+    'linebreak-style': 0,
+    'no-unexpected-multiline': 'warn',
+    'keyword-spacing': 1,
+    'comma-dangle': 1,
+    'comma-spacing': 1,
+    'arrow-spacing': 1
+  }
+}
