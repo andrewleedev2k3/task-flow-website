dbb83ccc0b6c1e4d4c71004204454ea1327f1423
andrewleedev2k3 <andrewleedev276@gmail.com>
2024-03-17 21:45:44 +0700

    Add Theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Button from '@mui/material/Button'
 import AbcIcon from '@mui/icons-material/Abc'
+import Typography from '@mui/material/Typography'
 
 const App = () => {
   return (
     <div>
+      <Typography variant="body2" color="text.secondary">
+        Text Seconnary
+      </Typography>
       <Button variant="contained">Hello world</Button>
       <AbcIcon color="secondary" />
     </div>
