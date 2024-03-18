f74acdcfa551491554f399eb86ee8dc8b192aebb
andrewleedev2k3 <andrewleedev276@gmail.com>
2024-03-18 09:09:48 +0700

    Add Darkmode

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '@fontsource/roboto'
 import CssBaseline from '@mui/material/CssBaseline'
-import { ThemeProvider } from '@emotion/react'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
 import theme from './theme.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
+    <CssVarsProvider theme={theme}>
       <CssBaseline />
       <App />
-    </ThemeProvider>
+    </CssVarsProvider>
   </React.StrictMode>
 )
