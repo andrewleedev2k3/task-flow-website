294bde3c671e7bc7f0895863dc8bf6b4b65251d0
andrewleedev2k3 <andrewleedev276@gmail.com>
2024-03-18 10:45:03 +0700

    Refactor Project Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,10 @@
-import { useColorScheme } from '@mui/material/styles'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select, { SelectChangeEvent } from '@mui/material/Select'
-import Box from '@mui/material/Box'
-import { DarkModeOutlined, LightModeOutlined, SettingsBrightness } from '@mui/icons-material'
-import Container from '@mui/material/Container'
-function ModeSelect() {
-  const { mode, setMode } = useColorScheme()
-  type typeMode = typeof mode & null
-  const handleChange = (event: SelectChangeEvent) => {
-    const selectedMode = event.target.value
-    if (selectedMode) {
-      setMode(selectedMode as typeMode)
-    }
-  }
-
-  return (
-    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-      <InputLabel id="label-select-dark-light-mode">Age</InputLabel>
-      <Select
-        labelId="label-select-dark-light-mode"
-        id="label-select-dark-light-mode"
-        value={mode}
-        label="Mode"
-        onChange={handleChange}
-      >
-        <MenuItem value="light">
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-            <LightModeOutlined fontSize="small" />
-            Light
-          </Box>
-        </MenuItem>
-        <MenuItem value="dark">
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-            <DarkModeOutlined fontSize="small" />
-            Dark
-          </Box>
-        </MenuItem>
-        <MenuItem value="system">
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-            <SettingsBrightness fontSize="small" />
-            System
-          </Box>
-        </MenuItem>
-      </Select>
-    </FormControl>
-  )
-}
+import Board from './pages/Boards/_id'
 
 const App = () => {
   return (
-    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
-      <Box
-        sx={{
-          backgroundColor: 'primary.light',
-          width: '100%',
-          height: (theme) => theme.app.appBarHeight,
-          display: 'flex',
-          alignItems: 'center'
-        }}
-      >
-        <ModeSelect />
-      </Box>
-
-      <Box
-        sx={{
-          backgroundColor: 'primary.dark',
-          width: '100%',
-          height: (theme) => theme.app.boardBarHeight,
-          display: 'flex',
-          alignItems: 'center'
-        }}
-      >
-        ModeBar
-      </Box>
-      <Box
-        sx={{
-          backgroundColor: 'primary.main',
-          width: '100%',
-          height: (theme) =>
-            `calc(100vh - ${theme.app.appBarHeight} - ${theme.app.boardBarHeight})`,
-          display: 'flex',
-          alignItems: 'center'
-        }}
-      >
-        Board Content
-      </Box>
-    </Container>
+    <>
+      <Board />
+    </>
   )
 }
 
---
+++ src/components/ModeSelect/index.tsx
@@ -0,0 +1,52 @@
+import { useColorScheme } from '@mui/material/styles'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+import Box from '@mui/material/Box'
+import { DarkModeOutlined, LightModeOutlined, SettingsBrightness } from '@mui/icons-material'
+
+const ModeSelect = () => {
+  const { mode, setMode } = useColorScheme()
+  type typeMode = typeof mode & null
+  const handleChange = (event: SelectChangeEvent) => {
+    const selectedMode = event.target.value
+    if (selectedMode) {
+      setMode(selectedMode as typeMode)
+    }
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Age</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="label-select-dark-light-mode"
+        value={mode}
+        label="Mode"
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeOutlined fontSize="small" />
+            Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeOutlined fontSize="small" />
+            Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightness fontSize="small" />
+            System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+export default ModeSelect
