803dbda81e901cb01de2f8e102c544894e5b1bb8
andrewleedev2k3 <andrewleedev276@gmail.com>
2024-03-23 17:53:08 +0700

    Add cross-env

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,10 @@
-export const API_ROOT = 'http://localhost:3000'
-// export const API_ROOT = 'https://task-flow-api-541w.onrender.com'
+let apiRoot = ''
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:3000'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://task-flow-api-541w.onrender.com'
+}
+
+export const API_ROOT = apiRoot
